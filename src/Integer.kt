d07ade1c65569930509ff9444fc03aaf681b84e8
Tiago Gomes <tidgomes@gmail.com>
2026-03-02 12:42:06 -0300

    input a number

--- src/Integer.kt
+++ src/Integer.kt
@@ -1,6 +1,9 @@
 fun main() {
-    val apples: Int = 5;
-    val oranges: Int = apples + 7;
+    println("How many apples John has?")
+    val apples: Int = readln().toInt();
+
+    println("How many oranges Fred has?")
+    val oranges: Int = readln().toInt();
 
     println("Number of fruits: ${apples + oranges}")
 }
